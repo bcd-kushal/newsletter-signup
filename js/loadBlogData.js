6d7568b47c545ff56939809b19ce6d6d93465b28
liumstone <am.kushal02@gmail.com>
2023-11-18 00:01:26 +0530

    Add files via upload

---
+++ js/loadBlogData.js
@@ -0,0 +1,127 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { ref, child, get } from "firebase/database";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBO2LjF2Vau4wAhjiSB6i-xpUfIrWMv67w",
+    authDomain: "newsletter-5be1e.firebaseapp.com",
+    databaseURL: "https://newsletter-5be1e-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "newsletter-5be1e",
+    storageBucket: "newsletter-5be1e.appspot.com",
+    messagingSenderId: "422358715800",
+    appId: "1:422358715800:web:58f50be1736602d1be6fed"
+};
+
+const app = initializeApp(firebaseConfig);
+const dataBase = getDatabase(app);
+
+
+function convertToProperDate(inputString) {
+    // Parse the input string into a Date object
+    const dateObj = inputString.substring(0,10);
+    const dateObject = new Date(dateObj);
+  
+    const day = dateObject.getDate();
+    const month = dateObject.toLocaleString('default', { month: 'short' });
+    const year = dateObject.getFullYear().toString().slice(-2);
+
+    const formattedDate = `${day} ${month} ${year}`;
+
+    return formattedDate;
+}
+
+
+
+//=======[ add blogs from database on window load ]========================================
+
+(() => {
+
+
+    const blogContainerBox = document.getElementById("blogContainer");
+
+
+    get( child( ref( dataBase ), "myBlogs/"))
+        .then(( data ) => {
+            if( data.exists() ){
+
+                const allBlogs = data.val();
+                
+                
+                /* 
+                Object.entries(allBlogs).forEach(([key, value]) => {
+                    console.log(`Key: ${key}, Value: ${value.title} ${value.desc}`);
+                });                  
+                */
+
+
+                Object.entries(allBlogs).forEach(([key, value]) => {
+                    
+                    blogContainerBox.appendChild(createHeaderDescPair());
+
+                    updateBlogDataFromDB({
+                        title: value.title,
+                        desc: value.desc,
+                        date: key
+                    });
+                    
+                });
+
+
+
+
+            } else {
+                console.error( "couldnt get blog data from firebase ");
+            }
+
+        })
+        .catch((err) => {
+            console.error("ERROR 204: ", err);
+        });
+    
+
+})();
+
+//================[ utility funcs ]========================================================
+
+function createHeaderDescPair() {
+    const element = document.querySelector('[data-firebase-blog-template]').content.cloneNode(true);
+    return element;
+}
+
+
+function updateBlogDataFromDB( blogData ){
+
+    const len = document.querySelectorAll("[data-title-box]").length || 0;
+
+
+    //------- these blogs are already saved to database ------------------->
+    
+    document.querySelectorAll("[data-title-box]")[len-1].value = String( blogData.title ) || "undefined_title";
+    document.querySelectorAll("[data-time-box]")[len-1].innerHTML = convertToProperDate( String( blogData.date ) ) || "undefined_date";
+
+    const x = document.querySelectorAll("[data-desc-box]")[len-1];
+    x.value = String( blogData.desc ) || "undefined_description_field";
+
+    var lineHeight = parseFloat(getComputedStyle(x).lineHeight);
+    var rows = x.value.split('\n').length;
+    var remainingEmptyHeight = (rows - 1) * lineHeight;
+
+    // Shrink the textarea
+    x.style.height = `${remainingEmptyHeight}px`;
+    console.log(remainingEmptyHeight);
+    
+    x.style.resize = "none";
+    
+    document.querySelectorAll("[data-title-box]")[len-1].style.cursor = "pointer";
+    document.querySelectorAll("[data-desc-box]")[len-1].style.cursor = "pointer";
+    document.querySelectorAll("[data-header-desc-pair]")[len-1].style.cursor = "pointer";
+
+
+
+
+    document.querySelectorAll("[data-title-box]")[len-1].readOnly = true;
+    document.querySelectorAll("[data-desc-box]")[len-1].readOnly = true;
+
+
+}
+
